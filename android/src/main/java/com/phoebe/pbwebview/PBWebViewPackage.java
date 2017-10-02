6a80a0cf01240761c384e7b422468db51f742235
Nguyen Duc Binh <binhnd@socicom.com.vn>
2017-10-02 17:46:20 +0700

    [Android] Correct build on ReactNative@48

--- android/src/main/java/com/phoebe/pbwebview/PBWebViewPackage.java
+++ android/src/main/java/com/phoebe/pbwebview/PBWebViewPackage.java
@@ -19,7 +19,7 @@ public class PBWebViewPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
