b4b95112d61598d10e2d315fd5c8d9df78e3cf5e
Nguyen Duc Binh <binhnd@socicom.com.vn>
2018-01-05 16:16:59 +0700

    Correct propTypes importing

--- WKWebView.ios.js
+++ WKWebView.ios.js
@@ -9,7 +9,8 @@ import ReactNative, {
   View,
   NativeModules,
   Text,
-  ActivityIndicator
+  ActivityIndicator,
+  ViewPropTypes,
 } from 'react-native';
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
 import deprecatedPropType from 'react-native/Libraries/Utilities/deprecatedPropType';
@@ -79,7 +80,7 @@ var WKWebView = createReactClass({
     NavigationType: NavigationType,
   },
   propTypes: {
-    ...View.propTypes,
+    ...ViewPropTypes,
 
     html: deprecatedPropType(
       PropTypes.string,
@@ -178,7 +179,7 @@ var WKWebView = createReactClass({
     onNavigationStateChange: PropTypes.func,
     scalesPageToFit: PropTypes.bool,
     startInLoadingState: PropTypes.bool,
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
     /**
      * Sets the JS to be injected when the webpage loads.
      */
